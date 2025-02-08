44ca50655a427f5d675f7ba9cb6e993f57e3ad0b
Jyendra <jyendra@astiostech.com>
2025-02-09 03:01:01 +0800

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,33 +32,45 @@ export default function App() {
   };
 
   return (
-    
-      <div className="min-h-screen flex flex-col items-center justify-center bg-green-900 text-white p-4">
+
+    <div className="min-h-screen flex flex-col items-center justify-center bg-green-900 text-white p-4">
       <h1 className="text-3xl font-bold mb-6">Playing Card Distributor</h1>
       <div className="bg-gray-800 p-6 rounded-lg shadow-lg w-96">
         <form onSubmit={handleSubmit} className="space-y-4">
-          <input
-            type="number"
-            value={numPeople}
-            onChange={(e) => setNumPeople(Number(e.target.value))}
-            placeholder="Enter number of players"
-            className="border p-2 w-full text-white rounded"
-          />
+          <div className="mb-4" style={{ display: 'flex', flexDirection: 'row' }}>
+
+            <label >
+              Total number of Players :
+              <input
+                type="number"
+                value={numPeople}
+                onChange={(e) => setNumPeople(Number(e.target.value))}
+                placeholder="Enter number of players"
+                className="border p-2 w-full text-white rounded"
+              />
+            </label>
+          </div>
           <button type="submit" className="bg-red-600 hover:bg-red-700 p-2 w-full rounded text-black font-bold">
             Deal Cards
           </button>
         </form>
         {error && <p className="text-yellow-300 mt-2">{error}</p>}
+        
         <div className="mt-4 space-y-2">
-          {distribution.map((cards, index) => (
-            <div key={index} className="bg-black p-3 rounded shadow-md text-center text-lg font-semibold">
-              {cards}
-            </div>
-          ))}
+        
+
+           {distribution.map((cards, index) => (
+            <>
+              <p className="text-lg font-bold">Player {index + 1}</p>
+              <div key={index} className="bg-black p-3 rounded shadow-md text-center text-lg font-semibold">
+                {cards}
+              </div>
+            </>
+          ))} 
         </div>
       </div>
     </div>
-    
+
   )
 }
 
